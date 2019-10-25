ba58a400739599d6a4382787a40dc8518c3a74a3
yulik <615972481@qq.com>
2019-10-25 11:28:16 +0800

    创建项目

---
+++ src/main/java/com/ahhswag/AhhswagApplication.java
@@ -0,0 +1,13 @@
+package com.ahhswag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhhswagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhhswagApplication.class, args);
+    }
+
+}
